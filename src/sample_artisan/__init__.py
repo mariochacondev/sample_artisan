8e73e775156d833d14d2d658b6f3ebb43c64d1cb
Mario Chacon <65020644+mariochacondev@users.noreply.github.com>
2026-06-03 16:29:06 +0200

    Export sample generator API

--- src/sample_artisan/__init__.py
+++ src/sample_artisan/__init__.py
@@ -1,7 +1,7 @@
 """Public package interface for sample_artisan."""
 
-from sample_artisan.core import craft_sample
+from sample_artisan.core import DEFAULT_SAMPLE_RATE, generate_wave_sample
 
-__all__ = ["craft_sample"]
+__all__ = ["DEFAULT_SAMPLE_RATE", "generate_wave_sample"]
 
 __version__ = "0.1.0"
