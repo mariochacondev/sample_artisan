42e4862896359ca5268c9c9a0f9fb807b440cb63
Mario Chacon <65020644+mariochacondev@users.noreply.github.com>
2026-06-04 15:26:35 +0200

    Expose advanced synth patch API

--- src/sample_artisan/__init__.py
+++ src/sample_artisan/__init__.py
@@ -1,7 +1,7 @@
 """Public package interface for sample_artisan."""
 
-from sample_artisan.core import DEFAULT_SAMPLE_RATE, generate_wave_sample
+from sample_artisan.core import DEFAULT_SAMPLE_RATE, SynthPatch, generate_wave_sample, render_patch
 
-__all__ = ["DEFAULT_SAMPLE_RATE", "generate_wave_sample"]
+__all__ = ["DEFAULT_SAMPLE_RATE", "SynthPatch", "generate_wave_sample", "render_patch"]
 
 __version__ = "0.1.0"
